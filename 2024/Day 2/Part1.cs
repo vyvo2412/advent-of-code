5df293365282a1bc7955be2cccbc1a543cef0c5a
Vy Vo <vxv@tenaska.com>
2024-12-16 15:45:30 -0600

    Day 2

---
+++ 2024/Day 2/Part1.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode._2024.Day2
+{
+    public class Part1
+    {
+        public void Process(string fileLocation) 
+        {
+            var total = 0;
+
+            foreach (var line in File.ReadLines(fileLocation))
+            {
+                var numbers = line.Split(" ").Select(int.Parse);
+                var previousOrder = Order.Starting; 
+                var currentOrder = Order.Neither;
+                var safe = true;
+
+                for (int i = 0; i < numbers.Count() - 1; i++)
+                {
+                    var pace = Math.Abs(numbers.ElementAt(i) - numbers.ElementAt(i + 1));
+
+                    if (numbers.ElementAt(i) > numbers.ElementAt(i + 1))
+                    {
+                        currentOrder = Order.Decrease;
+                    }
+                    else if (numbers.ElementAt(i) < numbers.ElementAt(i + 1))
+                    {
+                        currentOrder = Order.Increase;
+                    }
+                    else
+                    {
+                        currentOrder = Order.Neither;
+                    }
+
+                    if ((previousOrder == Order.Starting || currentOrder != Order.Neither && previousOrder == currentOrder) && pace <= 3)
+                    {
+                        previousOrder = currentOrder;
+                    }
+                    else
+                    {
+                        safe = false;
+                        break;
+                    }
+                }
+                
+                if (safe)
+                {
+                    total++;
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+
+        public enum Order
+        {
+            Increase,
+            Decrease,
+            Neither,
+            Starting
+        }
+    }
+}
---
+++ 2024/Day 2/Part2.cs
@@ -0,0 +1,78 @@
+﻿namespace AdventOfCode._2024.Day2
+{
+    public class Part2
+    {
+        public void Process(string fileLocation) 
+        {
+            var total = 0;
+
+            foreach (var line in File.ReadLines(fileLocation))
+            {
+                var numbers = line.Split(" ").Select(int.Parse).ToList();
+
+                if (CheckLevels(numbers))
+                {
+                    total++;
+                }
+                else
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        var newList = numbers.ToList();
+                        newList.RemoveAt(i);
+                        
+                        if (CheckLevels(newList))
+                        {
+                            total++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(total);
+        }
+
+        private static bool CheckLevels(List<int> numbers)
+        {
+            var previousOrder = Order.Starting;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                var pace = Math.Abs(numbers.ElementAt(i) - numbers.ElementAt(i + 1));
+
+                Order currentOrder;
+                if (numbers.ElementAt(i) > numbers.ElementAt(i + 1))
+                {
+                    currentOrder = Order.Decrease;
+                }
+                else if (numbers.ElementAt(i) < numbers.ElementAt(i + 1))
+                {
+                    currentOrder = Order.Increase;
+                }
+                else
+                {
+                    currentOrder = Order.Neither;
+                }
+
+                if ((previousOrder == Order.Starting || currentOrder != Order.Neither && previousOrder == currentOrder) && pace <= 3)
+                {
+                    previousOrder = currentOrder;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public enum Order
+        {
+            Increase,
+            Decrease,
+            Neither,
+            Starting
+        }
+    }
+}
