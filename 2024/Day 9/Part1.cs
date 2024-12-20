ee69d929c4bc011c36e69de2686fa4a31a8b2df0
Vy Vo <vxv@tenaska.com>
2024-12-19 20:24:42 -0600

    Day 9

---
+++ 2024/Day 9/Part1.cs
@@ -0,0 +1,73 @@
+﻿namespace AdventOfCode._2024.Day9
+{
+    public class Part1
+    {
+        private string _inputText;
+        private List<string> _blocks = [];
+
+        public void Process(string fileLocation)
+        {
+            ParseInput(fileLocation);
+            var numbers = _inputText.Trim().Select(c => int.Parse(c.ToString()));
+            // var numbers = new List<int> {2,3,3,3,1,3,3,1,2,1,4,1,4,1,3,1,4,0,2};
+            bool isNum = true;
+            var numTracking = 0;
+
+            foreach (var num in numbers)
+            {
+                if (num == 0)
+                {
+                    isNum = !isNum;
+                    continue;
+                }
+
+                var block = isNum ? numTracking.ToString() : ".";
+
+                for (int i = 0; i < num; i++)
+                {
+                    _blocks.Add(block);
+                }
+
+                numTracking = isNum ? numTracking + 1 : numTracking;
+                isNum = !isNum;
+            }
+
+            var end = _blocks.Count - 1;
+
+            for (int i = 0; i <= end; i++)
+            {
+                if (!int.TryParse(_blocks[i], out _))
+                {
+                    while (true)
+                    {
+                        if (int.TryParse(_blocks[end], out int replaceNum))
+                        {
+                            _blocks[i] = replaceNum.ToString();
+                            _blocks[end] = ".";
+                            break; 
+                        }
+
+                        end--;
+                    }
+                }
+            }
+
+            long total = 0;
+
+            for (int i = 0; i < _blocks.Count; i++)
+            {
+                if (_blocks[i] != ".")
+                {
+                    total += i * int.Parse(_blocks[i]);      
+                }                
+            }
+
+            Console.WriteLine(total);
+        }
+
+        private void ParseInput(string fileLocation)
+        {
+            _inputText = File.ReadLines(fileLocation).First();
+        }
+    }
+}
---
+++ 2024/Day 9/Part2.cs
@@ -0,0 +1,112 @@
+﻿
+namespace AdventOfCode._2024.Day9
+{
+    public class Part2
+    {
+        private string _inputText;
+        private List<string> _blocks = [];
+
+        public void Process(string fileLocation)
+        {
+            ParseInput(fileLocation);
+            var numbers = _inputText.Trim().Select(c => int.Parse(c.ToString()));
+            // var numbers = new List<int> { 2, 3, 3, 3, 1, 3, 3, 1, 2, 1, 4, 1, 4, 1, 3, 1, 4, 0, 2 };
+
+            bool isNum = true;
+            int numTracking = 0;
+
+            foreach (var num in numbers)
+            {
+                if (num == 0)
+                {
+                    isNum = !isNum;
+                    continue;
+                }
+
+                string block = isNum ? numTracking.ToString() : ".";
+
+                for (int x = 0; x < num; x++)
+                {
+                    _blocks.Add(block);
+                }
+
+                if (isNum) numTracking++;
+                isNum = !isNum;
+            }
+
+            var files = new List<(int Start, int Length, int FileId)>();
+            var freeSpaces = new List<(int Start, int Length)>();
+
+            int i = 0;
+            while (i < _blocks.Count)
+            {
+                if (_blocks[i] == ".")
+                {
+                    int start = i;
+                    while (i < _blocks.Count && _blocks[i] == ".") i++;
+                    freeSpaces.Add((start, i - start));
+                }
+                else
+                {
+                    int start = i;
+                    string fileIdStr = _blocks[i];
+                    while (i < _blocks.Count && _blocks[i] == fileIdStr) i++;
+                    files.Add((start, i - start, int.Parse(fileIdStr)));
+                }
+            }
+
+            files.Sort((a, b) => b.FileId.CompareTo(a.FileId));
+
+            foreach (var file in files)
+            {
+                bool moved = false;
+
+                foreach (var space in freeSpaces)
+                {
+                    if (space.Length >= file.Length && space.Start < file.Start)
+                    {
+                        for (int j = 0; j < file.Length; j++)
+                        {
+                            _blocks[space.Start + j] = file.FileId.ToString();
+                            _blocks[file.Start + j] = ".";
+                        }
+
+                        int newFreeStart = space.Start + file.Length;
+                        int newFreeLength = space.Length - file.Length;
+                        freeSpaces.Remove(space);
+                        if (newFreeLength > 0)
+                        {
+                            freeSpaces.Add((newFreeStart, newFreeLength));
+                        }
+
+                        freeSpaces.Sort((a, b) => a.Start.CompareTo(b.Start));
+
+                        moved = true;
+                        break;
+                    }
+                }
+
+                if (!moved)
+                {
+                    continue;
+                }
+            }
+
+            long total = 0;
+            for (int idx = 0; idx < _blocks.Count; idx++)
+            {
+                if (_blocks[idx] != ".")
+                {
+                    total += idx * int.Parse(_blocks[idx]);
+                }
+            }
+
+            Console.WriteLine($"Checksum: {total}");
+        }
+
+        private void ParseInput(string fileLocation)
+        {
+            _inputText = File.ReadLines(fileLocation).First();
+        }
+    }
+}
